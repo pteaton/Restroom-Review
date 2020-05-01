e60d21e295320c9929a40addb1555b278769b705
peaton373 <peaton373@gmail.com>
2020-05-01 12:00:19 -0500

    updated index.js, app.css, app.js, created reviewcontainer folder and index.js file added, review container renders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import ReviewContainer from './ReviewContainer'
 
 function App() {
   return (
     <div className="App">
-
+     
+      <ReviewContainer />
+    
     </div>
   );
 }
---
+++ src/ReviewContainer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ReviewContainer extends Component {
+	
+	render() {
+		
+		return(
+			<h3> ReviewContainer </h3>
+		)
+	}
+}
