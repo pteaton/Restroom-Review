b3ad8e6e8e612b79f73578f452a4b36b30dbd48b
peaton373 <peaton373@gmail.com>
2020-05-01 13:01:09 -0500

    created newreviewform and updated it as well as app.js

---
+++ src/NewReviewForm/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { Form, Button, Label, Segment } from 'semantic-ui-react'
+
+export default class NewReviewForm extends Component {
+
+	constructor() {
+		super(props)
+
+		this.state = {
+			title: '',
+			review: '',
+			location: '',
+		}
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.valu
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.props.createReview(this.state)
+
+		this.setState({
+			title: '',
+			review: '',
+			location: '',
+			posted_by: ''
+		})
+	}
+
+	render() {
+		return(
+			<Segment>
+				<h3> Add new review:</h3>
+				<Form onSubmit={this.handleSubmit} >
+					<Label>Title:</Label>
+					<Form.Input
+						type="text"
+						name="title"
+						value={this.state.title}
+						placeholder="Title your review"
+						onChange={this.handleChange}
+					/>
+					<Label>Review:</Label>
+					<Form.Input
+						type="text"
+						name="review"
+						value={this.state.review}
+						placeholder="Type a review"
+						onChange={this.handleChange}
+					/>
+					<Label>Location:</Label>
+					<Form.Input
+						type="text"
+						name="location"
+						value={this.state.location}
+						placeholder="Enter a location"
+						onChange={this.handleChange}
+					/>
+					<Button type="Submit">Create Review</Button>
+				</Form>
+			</Segment>
+		)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/ReviewContainer/index.js
+++ src/ReviewContainer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import NewDogForm from '../NewDogForm'
 
 export default class ReviewContainer extends Component {
 	
