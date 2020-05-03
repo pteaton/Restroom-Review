7d1bc5203ad5625b82b18e6459479c1de2ed7326
peaton373 <peaton373@gmail.com>
2020-05-02 19:13:47 -0500

    added editreviewmodal and added index.js file as well

---
+++ src/EditReviewModal/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { Form, Button, Label, Segment, Modal, Header } from 'semantic-ui-react'
+import '../index.css'
+
+export default class EditReviewModal extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			title: props.reviewToEdit.title,
+			review: props.reviewToEdit.review,
+			location: props.reviewToEdit.location
+		}
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			[event.target.title]: event.target.value
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.props.updateReview(this.state)
+	}
+
+	render() {
+		
+		return (
+			<Modal open={true} closeIcon={true} onClose={this.props.closeModal}>
+				<Header>
+					<h3> Enter updated info</h3>
+				</Header>
+				<Form onSubmit={this.handleSubmit}>
+					<Form.input
+						type="text"
+						name="title"
+						value={this.state.title}
+						placeholder="Enter a title"
+						onChange={this.handleChange}
+					/>
+					<Label>Review:</Label>
+					<Form.input	
+						type="text"
+						name="review"
+						value={this.state.review}
+						placeholder="Enter a review"
+						onChange={this.handleChange}
+					/>
+					<Label>Location:</Label>
+					<Form.input
+						type="text"
+						name="location"
+						value={this.state.location}
+						placeholder="Enter a location"
+						onChange={this.handleChange}
+					/>
+					<Modal.Actions>
+						<Button type="Submit">Update Review</Button>
+					</Modal.Actions>
+				</Form>
+			</Modal>
+		)
+	}
+}
